f68db3de5d78db0bde7ce51e3f393411695494b0
robbridges <robbridges@users.noreply.github.com>
2021-02-25 20:10:33 -0800

    few more updates to inports

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Navlink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <header>
       <nav className ='main-nav'>
         <ul>
-          <li><Navlink to='/oceans'>Oceans</Navlink></li>
-          <li><Navlink to='/moutains'>Moutains</Navlink></li>
-          <li><Navlink to='/campfires'>Campfires</Navlink></li>
+          <li><NavLink to='/oceans'>Oceans</NavLink></li>
+          <li><NavLink to='/moutains'>Moutains</NavLink></li>
+          <li><NavLink to='/campfires'>Campfires</NavLink></li>
         </ul>
       </nav>
     </header>

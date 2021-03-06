c85e0c4999f032559d01fdd25848e19ea572520c
robbridges <robbridges@users.noreply.github.com>
2021-03-05 18:02:06 -0800

    rendered routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 
 import './css/index.css';
 import React, {Component} from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import axios from 'axios';
 import apiKey from './config';
 import SearchBar from './components/SearchBar';
 import Navigation from './components/Navigation';
-import PhotoContainer from './components/PhotoContainer'
+import PhotoContainer from './components/PhotoContainer';
+import PageNotFound from './components/PageNotFound';
+
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      photos: []
+      photos: [],
+      oceanPhotos: [],
+      moutainPhotos: [],
+      campfirePhotos: []
+
     };
   }
   
 componentDidMount() {
   this.getPhotos();
+
+  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=ocean&per_page=24&format=json&nojsoncallback=1`)
+    .then(res => {
+      this.setState({
+        oceanPhotos: res.data.photos.photo
+      });
+    })
+    .catch(err => {
+      console.log('Error fetching and parsing data', err);
+    });
+
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=moutain&per_page=24&format=json&nojsoncallback=1`)
+    .then(res => {
+      this.setState({
+        moutainPhotos: res.data.photos.photo
+      });
+    })
+    .catch(err => {
+      console.log('Error fetching and parsing data', err);
+    });
+
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=campfire&per_page=24&format=json&nojsoncallback=1`)
+    .then(res => {
+      this.setState({
+        campfirePhotos: res.data.photos.photo
+      });
+    })
+    .catch(err => {
+      console.log('Error fetching and parsing data', err);
+    });
+    
+    
 }
 
 
@@ -43,7 +81,14 @@ getPhotos = (query = 'moutains') => {
       <div className="App">
         <SearchBar onSearch={this.getPhotos}/>
         <Navigation />
-        <PhotoContainer data={this.state.photos} />
+        <Switch>
+          <Route exact path ="/" render = {() => <PhotoContainer data={this.state.photos} />} />
+          <Route path ="/search" render ={ () => <PhotoContainer data ={this.state.photos} onSearch={this.getPhotos} /> } />
+          <Route exact path="/oceans" render={ () => <PhotoContainer data={this.state.oceanPhotos} /> }/>
+          <Route exact path="/moutains" render={ () => <PhotoContainer data={this.state.moutainPhotos}  /> }/>
+          <Route exact path="/campfires" render={ () => <PhotoContainer data={this.state.campfirePhotos} /> }/>
+          <Route component={PageNotFound}/>
+        </Switch>
       </div>
     </BrowserRouter>
 
--- src/components/pagenotfound.js
+++ src/components/pagenotfound.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Navlink} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const PageNotFound = () => {
   return(
     <div>
       <h1> Page not found</h1>
       <h2>Yikes, how'd you get here? There's no page here!</h2>
-      <p><Navlink to='/'>Let's get you back to safety</Navlink></p>
+      <p><NavLink to='/'>Let's get you back to safety</NavLink></p>
     </div>
   );
 };

a615330d028f5d3e15be2d5d4dae5973b99939b9
robbridges <robbridges@users.noreply.github.com>
2021-03-08 14:20:29 -0800

    Add files via upload

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+/*
+Simple 404 page that if a route is requested that is not loacted it tells the user about the error and navigates them back to the home route of the app
+*/
+const PageNotFound = () => {
+  return(
+    <div>
+      <h1> Page not found</h1>
+      <h2>Yikes, how'd you get here? There's no page here!</h2>
+      <p><NavLink to='/'>Let's get you back to safety</NavLink></p>
+    </div>
+  );
+};
+
+export default PageNotFound;
